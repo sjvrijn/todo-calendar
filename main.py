c900d23b648d8a8a6c953dbcf5f1d572f26cb7df
Sander van Rijn <svr003@gmail.com>
2019-05-13 00:47:30 +0200

    default example rewritten to get current event from specific calendar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
 import pickle
 import os.path
+from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -9,7 +9,11 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
-def main():
+def asUTC(t):
+    return t.isoformat() + 'Z'  # 'Z' indicates UTC time
+
+
+def getcurrentevent(calendar_name):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -35,19 +39,21 @@ def main():
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
+    now = datetime.utcnow()
 
+    calendars = service.calendarList().list().execute().get('items', [])
+    cal = [c for c in calendars if c['summary'] == calendar_name][0]
+
+    cur_event = service.events().list(calendarId=cal['id'], orderBy='startTime',
+                                      maxResults=1, singleEvents=True,
+                                      timeMax=asUTC(now + timedelta(seconds=1)),
+                                      timeMin=asUTC(now)).execute()
+    events = cur_event.get('items', [])
     if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        raise Exception('No upcoming events found.')
+
+    return events[0]
 
 
 if __name__ == '__main__':
-    main()
+    getcurrentevent('Day-planning')
